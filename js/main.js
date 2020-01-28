feefbe9aa3027436213a318b090913e151f333bb
jonastheis <work@jonastheis.de>
2020-01-28 11:30:30 +0100

    Implement graph viewer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const ANALYSIS_SERVER_URL = "116.202.49.178" + "/datastream";
 const NODE_ID_LENGTH = 64;
 
+const EDGE_COLOR_DEFAULT = "#444444";
+const VERTEX_COLOR_DEFAULT = "0x666666";
+const VERTEX_SIZE = 14;
+
 class Frontend {
     setStatusMessage(msg) {
         document.getElementById("status").innerHTML = msg;
@@ -34,10 +38,10 @@ class Datastructure {
         }
     }
 
-    removeNode() {
+    removeNode(idA) {
         if(this.nodesOnline.has(idA)) {
             this.nodesOnline.delete(idA);
-            // TODO: graph.removeNode(idA);
+            this.app.graph.deleteNode(idA);
 
             this.app.setStreamStatusMessage("removeNode from onlinepool: " + idA);
         }
@@ -64,8 +68,7 @@ class Datastructure {
         // check if not in nodesOnline set
         if(!this.nodesOnline.has(idA)) {
             this.nodesOnline.add(idA);
-            // TODO: add to graph
-            // graph.addNode(idA);
+            this.app.graph.addVertex(idA);
 
             this.app.setStreamStatusMessage("setNodeOnline: " + idA)
         } else {
@@ -97,8 +100,7 @@ class Datastructure {
         // check if node is currently online
         if(this.nodesOnline.has(idA)) {
             this.nodesOnline.delete(idA);
-            // TODO: remove from graph
-            // graph.removeNode(idA);
+            this.app.graph.deleteVertex(idA);
 
             this.app.setStreamStatusMessage("removedFromOnlinepool: " + idA)
         }
@@ -121,7 +123,7 @@ class Datastructure {
         } else {
             // add new connection only if both nodes are online
             if(this.nodesOnline.has(idA) && this.nodesOnline.has(idB) && idA != idB) {
-                // TODO: graph.addLink(idA, idB, con);
+                this.app.graph.addEdge(con, idA, idB);
                 this.connections.add(con);
 
                 // TODO: add additional data structure for fast neighbor lookup
@@ -146,9 +148,8 @@ class Datastructure {
         }
 
         if(this.connections.has(con)) {
-            // TODO: remove connection in graph
-            // let conn = graph.getLink(idA, idB);
-            // graph.removeLink(conn);
+            this.connections.delete(con);
+            this.app.graph.deleteEdge(con, idA, idB);
 
             this.app.setStreamStatusMessage("disconnectNodes: " + idA + " > " + idB);
             this.app.updateStatus();
@@ -158,11 +159,65 @@ class Datastructure {
     }
 }
 
+class Graph {
+    constructor() {
+        this.graph = Viva.Graph.graph();
+        this.graphics = Viva.Graph.View.webglGraphics();
+        this.calculator = Viva.Graph.centrality();
+
+        this.layout = Viva.Graph.Layout.forceDirected(this.graph, {
+            springLength: 30,
+            springCoeff: 0.0001,
+            dragCoeff: 0.02,
+            gravity: -1.2
+        });
+
+        this.graphics.link((link) => {
+            return Viva.Graph.View.webglLine(EDGE_COLOR_DEFAULT);
+        });
+
+        this.graphics.setNodeProgram(buildCircleNodeShader());
+
+        this.graphics.node((node) => {
+            return new WebGLCircle(VERTEX_SIZE, VERTEX_COLOR_DEFAULT);
+        });
+
+        this.renderer = Viva.Graph.View.renderer(this.graph, {
+            layout: this.layout,
+            graphics: this.graphics,
+            container: document.getElementById('graphc'),
+            renderLinks: true
+        });
+    }
+
+    addEdge(con, idA, idB) {
+        this.graph.addLink(idA, idB, con);
+    }
+
+    deleteEdge(con, idA, idB) {
+        // TODO: verify let conn = graph.getLink(idA, idB);
+        this.graph.removeLink(con);
+    }
+
+    addVertex(idA) {
+        this.graph.addNode(idA);
+    }
+
+    deleteVertex(idA) {
+        this.graph.removeNode(idA);
+    }
+
+    render() {
+        this.renderer.run();
+    }
+}
+
 class Application {
     constructor(url) {
         this.url = url;
         this.frontend = new Frontend();
         this.ds = new Datastructure(this);
+        this.graph = new Graph();
 
         this.rendered = false; // is the application already rendered?
         this.floodNew = 0;
@@ -208,7 +263,7 @@ class Application {
         // kickoff rendering
         this.rendered = true;
 
-        // TODO: call renderer
+        this.graph.render();
         
         this.setStreamStatusMessage("... received " + this.floodNew + " msg");
         this.updateStatus();
@@ -291,4 +346,147 @@ class Application {
 
 window.onload = () => {
     new Application(ANALYSIS_SERVER_URL).run();
+}
+
+
+
+
+
+
+/**
+ * WebGL stuff 
+ */
+
+function WebGLCircle(size, color) {
+    this.size = size;
+    this.color = color;
+}
+// Next comes the hard part - implementation of API for custom shader
+// program, used by webgl renderer:
+function buildCircleNodeShader() {
+    // For each primitive we need 4 attributes: x, y, color and size.
+    var ATTRIBUTES_PER_PRIMITIVE = 4,
+        nodesFS = [
+            'precision mediump float;',
+            'varying vec4 color;',
+            'void main(void) {',
+            '   if ((gl_PointCoord.x - 0.5) * (gl_PointCoord.x - 0.5) + (gl_PointCoord.y - 0.5) * (gl_PointCoord.y - 0.5) < 0.25) {',
+            '     gl_FragColor = color;',
+            '   } else {',
+            '     gl_FragColor = vec4(0);',
+            '   }',
+            '}'].join('\n'),
+        nodesVS = [
+            'attribute vec2 a_vertexPos;',
+            // Pack color and size into vector. First elemnt is color, second - size.
+            // Since it's floating point we can only use 24 bit to pack colors...
+            // thus alpha channel is dropped, and is always assumed to be 1.
+            'attribute vec2 a_customAttributes;',
+            'uniform vec2 u_screenSize;',
+            'uniform mat4 u_transform;',
+            'varying vec4 color;',
+            'void main(void) {',
+            '   gl_Position = u_transform * vec4(a_vertexPos/u_screenSize, 0, 1);',
+            '   gl_PointSize = a_customAttributes[1] * u_transform[0][0];',
+            '   float c = a_customAttributes[0];',
+            '   color.b = mod(c, 256.0); c = floor(c/256.0);',
+            '   color.g = mod(c, 256.0); c = floor(c/256.0);',
+            '   color.r = mod(c, 256.0); c = floor(c/256.0); color /= 255.0;',
+            '   color.a = 1.0;',
+            '}'].join('\n');
+    var program,
+        gl,
+        buffer,
+        locations,
+        utils,
+        nodes = new Float32Array(64),
+        nodesCount = 0,
+        canvasWidth, canvasHeight, transform,
+        isCanvasDirty;
+    return {
+        /**
+         * Called by webgl renderer to load the shader into gl context.
+         */
+        load: function (glContext) {
+            gl = glContext;
+            webglUtils = Viva.Graph.webgl(glContext);
+            program = webglUtils.createProgram(nodesVS, nodesFS);
+            gl.useProgram(program);
+            locations = webglUtils.getLocations(program, ['a_vertexPos', 'a_customAttributes', 'u_screenSize', 'u_transform']);
+            gl.enableVertexAttribArray(locations.vertexPos);
+            gl.enableVertexAttribArray(locations.customAttributes);
+            buffer = gl.createBuffer();
+        },
+        /**
+         * Called by webgl renderer to update node position in the buffer array
+         *
+         * @param nodeUI - data model for the rendered node (WebGLCircle in this case)
+         * @param pos - {x, y} coordinates of the node.
+         */
+        position: function (nodeUI, pos) {
+            var idx = nodeUI.id;
+            nodes[idx * ATTRIBUTES_PER_PRIMITIVE] = pos.x;
+            nodes[idx * ATTRIBUTES_PER_PRIMITIVE + 1] = -pos.y;
+            nodes[idx * ATTRIBUTES_PER_PRIMITIVE + 2] = nodeUI.color;
+            nodes[idx * ATTRIBUTES_PER_PRIMITIVE + 3] = nodeUI.size;
+        },
+        /**
+         * Request from webgl renderer to actually draw our stuff into the
+         * gl context. This is the core of our shader.
+         */
+        render: function () {
+            gl.useProgram(program);
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, nodes, gl.DYNAMIC_DRAW);
+            if (isCanvasDirty) {
+                isCanvasDirty = false;
+                gl.uniformMatrix4fv(locations.transform, false, transform);
+                gl.uniform2f(locations.screenSize, canvasWidth, canvasHeight);
+            }
+            gl.vertexAttribPointer(locations.vertexPos, 2, gl.FLOAT, false, ATTRIBUTES_PER_PRIMITIVE * Float32Array.BYTES_PER_ELEMENT, 0);
+            gl.vertexAttribPointer(locations.customAttributes, 2, gl.FLOAT, false, ATTRIBUTES_PER_PRIMITIVE * Float32Array.BYTES_PER_ELEMENT, 2 * 4);
+            gl.drawArrays(gl.POINTS, 0, nodesCount);
+        },
+        /**
+         * Called by webgl renderer when user scales/pans the canvas with nodes.
+         */
+        updateTransform: function (newTransform) {
+            transform = newTransform;
+            isCanvasDirty = true;
+        },
+        /**
+         * Called by webgl renderer when user resizes the canvas with nodes.
+         */
+        updateSize: function (newCanvasWidth, newCanvasHeight) {
+            canvasWidth = newCanvasWidth;
+            canvasHeight = newCanvasHeight;
+            isCanvasDirty = true;
+        },
+        /**
+         * Called by webgl renderer to notify us that the new node was created in the graph
+         */
+        createNode: function (node) {
+            nodes = webglUtils.extendArray(nodes, nodesCount, ATTRIBUTES_PER_PRIMITIVE);
+            nodesCount += 1;
+        },
+        /**
+         * Called by webgl renderer to notify us that the node was removed from the graph
+         */
+        removeNode: function (node) {
+            if (nodesCount > 0) { nodesCount -= 1; }
+            if (node.id < nodesCount && nodesCount > 0) {
+                // we do not really delete anything from the buffer.
+                // Instead we swap deleted node with the "last" node in the
+                // buffer and decrease marker of the "last" node. Gives nice O(1)
+                // performance, but make code slightly harder than it could be:
+                webglUtils.copyArrayPart(nodes, node.id * ATTRIBUTES_PER_PRIMITIVE, nodesCount * ATTRIBUTES_PER_PRIMITIVE, ATTRIBUTES_PER_PRIMITIVE);
+            }
+        },
+        /**
+         * This method is called by webgl renderer when it changes parts of its
+         * buffers. We don't use it here, but it's needed by API (see the comment
+         * in the removeNode() method)
+         */
+        replaceProperties: function (replacedNode, newNode) { },
+    };
 }
